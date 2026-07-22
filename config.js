d49a96300b908cdbaa38119279ae005aaf336e72
Masato Yasuda <296491634+yasu369@users.noreply.github.com>
2026-07-23 05:09:53 +0900

    Update contact form email

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.ONCLINIC_CONFIG = Object.freeze({
   lineOfficialUrl: "https://lin.ee/PNBQpOa",
   noteRssUrl: "https://note.com/onn_kuri/rss",
   noteNewsJsonUrl: "news.json",
-  contactEmail: "info@double-k.co.jp",
+  contactEmail: "contact@onn-clinic.jp",
   reservationSubject: "オンライン診療予約"
 });
